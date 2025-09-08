85b939710c4e01c437832131c6e77fe3e93cbce3
Matvei <matveiyan@yandex.ru>
2025-09-08 18:30:47 +0300

    frontend docker image

--- frontend/src/init.ts
+++ frontend/src/init.ts
@@ -1,4 +1,4 @@
 export const AppVersion = '0.0.1'
 export const wsUrl = import.meta.env.DEV
   ? "ws://localhost:8001/ws"
-  : `wss://${window.location.host}/ws`
+  : `${window.location.protocol === "https:" ? "wss" : "ws"}://${window.location.host}/ws`
