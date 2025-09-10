ed297558551273ad972af0e55510669e01bd1471
Matvei <matveiyan@yandex.ru>
2025-09-10 20:26:12 +0300

    change ci/cd pipeline

--- frontend/src/init.ts
+++ frontend/src/init.ts
@@ -1,4 +1,4 @@
-export const AppVersion = '0.0.1'
+export const AppVersion = '0.0.2'
 export const wsUrl = import.meta.env.DEV
   ? "ws://localhost:8001/ws"
   : `${window.location.protocol === "https:" ? "wss" : "ws"}://${window.location.host}/ws`
