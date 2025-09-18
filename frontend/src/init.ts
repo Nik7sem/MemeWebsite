deb38a3b378f1c95d02e787cd195c33ea06e6b88
Matvei <matveiyan@yandex.ru>
2025-09-19 00:25:19 +0300

    update version

--- frontend/src/init.ts
+++ frontend/src/init.ts
@@ -1,4 +1,4 @@
-export const AppVersion = '0.2.0'
+export const AppVersion = '0.2.1'
 export const wsUrl = import.meta.env.DEV
   ? "ws://localhost:8001/ws"
   : `${window.location.protocol === "https:" ? "wss" : "ws"}://${window.location.host}/ws`
